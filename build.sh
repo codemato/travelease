3e0348a0b7854d4c62e883c9ae3a234d9e8ab603
mkunjir-tavisca <mkunjir@tavisca.com>
2024-07-31 19:25:11 +0530

    nohup added

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ cd /home/ubuntu/travelease/travelease
 kill -9 $(netstat -tulpn | grep -i python | grep -i 0.0.0.0:80 | awk  '{print $7}' | sed 's/\/python//g')
 git stash
 git pull
+chmod +x build.sh
 nohup streamlit run app.py --server.port 80 &
 echo "done"
