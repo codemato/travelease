4de4ff3e40825d80580d87600797ea3bf16efd94
mkunjir-tavisca <mkunjir@tavisca.com>
2024-07-31 18:22:25 +0530

    amadeus added

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 export PATH=$PATH:/home/ubuntu/travelease/travelease/bin
 cd /home/ubuntu/travelease/travelease
 kill -9 $(netstat -tulpn | grep -i python | grep -i 0.0.0.0:80 | awk  '{print $7}' | sed 's/\/python//g')
+git stash
 git pull
 streamlit run app.py --server.port 80
 echo "done"
